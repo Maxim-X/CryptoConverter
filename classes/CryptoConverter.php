4fd6dadb1aafe17a04e9d630bb64178c99419631
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2022-08-28 02:10:27 +0500

    Edit

--- classes/CryptoConverter.php
+++ classes/CryptoConverter.php
@@ -4,8 +4,55 @@
  */
 class CryptoConverter
 {
-	const PATH_CACHE = "cache/ticker.json";
+	const PATH_CACHE = "cache/ticker.json"; // Путь к кэшу
+	const TIME_UPDATE = 60; // Время обновления кэша в секундах
 
+	/**
+	 * Получение информации о курсе при обмене валют
+	 */
+	static public function search_ticker($inpCurrency, $outCurrency){
+		$inpCurrency = strtoupper($inpCurrency);
+		$outCurrency = strtoupper($outCurrency);
+		$reСonversion = false;
+
+		$json_file = self::read_ticker();
+
+		
+		if (isset($json_file[$inpCurrency][$outCurrency])) { // Проверяем наличие курса валюты в кэше
+			$curs = $json_file[$inpCurrency][$outCurrency];
+		}else if (isset($json_file[$outCurrency][$inpCurrency])) { // Проверяем наличие обратного курса валюты в кэше для обратной котировки
+			$curs = $json_file[$outCurrency][$inpCurrency];
+			$reСonversion = true;
+		}else{
+			$curs = self::tiker_update($inpCurrency, $outCurrency);
+			array($outCurrency => array("update" => time(), "price" => $ticker[$outCurrency]));
+			$curs = $curs[$outCurrency]['price'];
+			return array("status" => true, "curs" => $curs);
+		}
+
+		if (self::check_update($curs)) { // Проверяем необходимость обновления кэша
+			if ($reСonversion) {
+				$curs = self::tiker_update($outCurrency, $inpCurrency);
+				$curs = $curs[$inpCurrency];
+			}else{
+				$curs = self::tiker_update($inpCurrency, $outCurrency);
+				$curs = $curs[$outCurrency];
+			}
+		}
+
+		$curs = $curs['price'];
+
+		// Обратной котировки Е = 1 /Е
+		if ($reСonversion) {
+			$curs = 1 / $curs;
+		}
+
+		return array("status" => true, "curs" => $curs);
+	}
+
+	/**
+	 * Получение информации о курсе при обмене для нескольких валют
+	 */
 	static public function search_list_ticker($allCurrency){
 		$json_file = self::read_ticker();
 		$inpCurrency = strtoupper($allCurrency['inpCurrency']);
@@ -15,8 +62,10 @@ class CryptoConverter
 		$reСonversion = false;
 
 		$jsonInpCurrency = $json_file[$inpCurrency];
-		
-		if (isset($jsonInpCurrency[$outCurrencyListItemOne]) && isset($jsonInpCurrency[$outCurrencyListItemTwo])) {
+
+		// Проверка возможности обновления курса для нескольких валют за один запрос к API
+		if (isset($jsonInpCurrency[$outCurrencyListItemOne]) &&
+		    isset($jsonInpCurrency[$outCurrencyListItemTwo])) {
 			$curs_one = $jsonInpCurrency[$outCurrencyListItemOne];
 			$curs_two = $jsonInpCurrency[$outCurrencyListItemTwo];
 			
@@ -29,50 +78,15 @@ class CryptoConverter
 			}
 		}
 
+		// Обновление курса для нескольких валют
 		$all_curs[$inpCurrency][$outCurrencyListItemOne]['price'] = self::search_ticker($inpCurrency, $outCurrencyListItemOne)["curs"];
 		$all_curs[$inpCurrency][$outCurrencyListItemTwo]['price'] = self::search_ticker($inpCurrency, $outCurrencyListItemTwo)["curs"];
 		return $all_curs;
 	}
 
-	static public function search_ticker($inpCurrency, $outCurrency){
-		$inpCurrency = strtoupper($inpCurrency);
-		$outCurrency = strtoupper($outCurrency);
-		$reСonversion = false;
-
-		$json_file = self::read_ticker();
-		
-		if (isset($json_file[$inpCurrency][$outCurrency])) {
-			$curs = $json_file[$inpCurrency][$outCurrency];
-		}else if (isset($json_file[$outCurrency][$inpCurrency])) {
-			$curs = $json_file[$outCurrency][$inpCurrency];
-			$reСonversion = true;
-		}else{
-
-			$curs = self::tiker_update($inpCurrency, $outCurrency);
-			array($outCurrency => array("update" => time(), "price" => $ticker[$outCurrency]));
-			$curs = $curs[$outCurrency]['price'];
-			return array("status" => true, "curs" => $curs);
-		}
-
-		if (self::check_update($curs)) {
-			if ($reСonversion) {
-				$curs = self::tiker_update($outCurrency, $inpCurrency);
-				$curs = $curs[$inpCurrency];
-			}else{
-				$curs = self::tiker_update($inpCurrency, $outCurrency);
-				$curs = $curs[$outCurrency];
-			}
-		}
-
-		$curs = $curs['price'];
-		
-		if ($reСonversion) {
-			$curs = 1 / $curs;
-		}
-
-		return array("status" => true, "curs" => $curs);
-	}
-
+	/**
+	 * Обновление информации о курсе валют в кэше
+	 */
 	static public function tiker_update($inpCurrency, $outCurrency){
 		$get = self::get_ticker($inpCurrency, $outCurrency);
 		$return_new_ticker = array();
@@ -80,6 +94,7 @@ class CryptoConverter
 		if (isset($get['Response'])) {
 			return [];
 		}else{
+			// Обновление курса в кэше для нескольких валют
 			if (is_array($outCurrency)) {
 				foreach($outCurrency as $currency){
 					$new_ticker = array($currency => array("update" => time(), "price" => $get[$currency]));
@@ -89,14 +104,18 @@ class CryptoConverter
 				return array($inpCurrency => $return_new_ticker);
 			}
 
+			// Обновление курса в кэше для одной валюты
 			$new_ticker = array($outCurrency => array("update" => time(), "price" => $get[$outCurrency]));
 			self::ticker_save($new_ticker, $inpCurrency);
 			return $new_ticker;
 		}
 	}
 
+	/**
+	 * Сохранение информации о курсе валют в файл json
+	 */
 	static public function ticker_save($ticker, $inpCurrency){
-		$file = file_get_contents(self::PATH_CACHE);  // Открыть файл data.json
+		$file = file_get_contents(self::PATH_CACHE); 
 		$taskList = json_decode($file,TRUE);                
 		unset($file);
 		if (isset($taskList[$inpCurrency])) {
@@ -110,6 +129,9 @@ class CryptoConverter
 		return true;
 	}
 
+	/**
+	 * Получение информации о курсе валют по API
+	 */
 	static public function get_ticker($inpCurrency, $outCurrency){
 		if (is_array($outCurrency)) {
 			$curl = curl_init("https://cryptocalc.online/api/v1/price?fsym=".$inpCurrency."&tsyms=".implode(",", $outCurrency));
@@ -125,19 +147,25 @@ class CryptoConverter
 		return $get;
 	}
 
+	/**
+	 * Проверка актуальности курса валют в кэше
+	 */
+	static public function check_update($json_curs){
+		$now_time = time();
+		if ($now_time - $json_curs['update'] > self::TIME_UPDATE) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Чтение файла с кэшэм
+	 */
 	static public function read_ticker(){
 		$file = file_get_contents(self::PATH_CACHE);
 		$file = json_decode($file, true);
-
 		return $file;
 	}
 
-	static public function check_update($json_curs){
-		$now_time = time();
-		if ($now_time - $json_curs['update'] > 60) {
-			return true;
-		}
-
-		return false;
-	}
+	
 }
