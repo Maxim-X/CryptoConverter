d3ab026750e43c73f59c87b608f9bef2c5b3898a
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2022-08-27 20:19:18 +0500

    Edit

--- classes/CryptoConverter.php
+++ classes/CryptoConverter.php
@@ -5,11 +5,51 @@
 class CryptoConverter
 {
 	const PATH_CACHE = "cache/ticker.json";
+	// all => [{"inpCurrency" => "BTC", "outCurrency" => "USD"}, ]
 
-	// function __construct(argument)
-	// {
+	static public function search_list_ticker($allCurrency){
+		$reСonversion = false;
+		$json_file = self::read_ticker();
+		$list_update = array();
+		$list_add = array();
 
-	// }
+		foreach($allCurrency as &$currency){
+			$inpCurrency = $currency["inpCurrency"];
+			$outCurrency = $currency["outCurrency"];
+
+			if (isset($json_file[$inpCurrency][$outCurrency])) {
+				$curs = $json_file[$inpCurrency][$outCurrency];
+				if (self::check_update($curs)) {
+					array_push($list_update, $currency);
+				}
+			}else if(isset($json_file[$outCurrency][$inpCurrency])){
+				if (self::check_update($curs)) {
+					$buffer = $currency["inpCurrency"];
+					$currency["inpCurrency"] = $currency["outCurrency"];
+					$currency["outCurrency"] = $buffer;
+					array_push($list_update, $currency);
+				}
+			}else{
+				array_push($list_add, $currency);
+			}
+		}
+
+		$curs = self::all_tickers_save($list_update);
+		// self::all_tickers_save($list_add);
+		return array("status" => true, "curs" => $curs);
+		// foreach($list_update as $currency){
+
+		// }
+		// foreach($list_add as $currency){
+
+		// }
+		// echo "<pre>";
+		// echo "Добавить";
+		// var_dump($list_add);
+		// echo "Обновить";
+		// var_dump($list_update);
+		// echo "</pre>";
+	}
 
 	static public function search_ticker($inpCurrency, $outCurrency){
 		$inpCurrency = strtoupper($inpCurrency);
@@ -24,27 +64,26 @@ class CryptoConverter
 			$curs = $json_file[$outCurrency][$inpCurrency];
 			$reСonversion = true;
 		}else{
-			$get = self::get_ticker($inpCurrency, $outCurrency);
-			if (isset($get['Response'])) {
-				return [];
-			}else{
-				$new_ticker = array($outCurrency => array("update" => time(), "price" => $get[$outCurrency]) );
-				$dd = self::ticker_save($new_ticker, $inpCurrency);
-				return array("status" => true, "curs" => $get[$outCurrency]);
-			}
+			$curs = self::tiker_update($inpCurrency, $outCurrency);
+			$curs = $curs[$outCurrency]['price'];
+			return array("status" => true, "curs" => $curs);
+			// $get = self::get_ticker($inpCurrency, $outCurrency);
+			// if (isset($get['Response'])) {
+			// 	return [];
+			// }else{
+			// 	$new_ticker = array($outCurrency => array("update" => time(), "price" => $get[$outCurrency]) );
+			// 	$dd = self::ticker_save($new_ticker, $inpCurrency);
+			// 	return array("status" => true, "curs" => $get[$outCurrency]);
+			// }
 
 		}
 
 		if (self::check_update($curs)) {
 			if ($reСonversion) {
 				$curs = self::tiker_update($outCurrency, $inpCurrency);
-				echo "1";
-				var_dump($curs);
 				$curs = $curs[$inpCurrency];
 			}else{
 				$curs = self::tiker_update($inpCurrency, $outCurrency);
-				echo "2";
-				var_dump($curs);
 				$curs = $curs[$outCurrency];
 			}
 		}
@@ -60,13 +99,18 @@ class CryptoConverter
 	}
 
 	static public function tiker_update($inpCurrency, $outCurrency){
+		$time_start = microtime(true);
 		$get = self::get_ticker($inpCurrency, $outCurrency);
+		$time_end = microtime(true);
+			$time = $time_end - $time_start;
+			echo "<br>".$time;
 		if (isset($get['Response'])) {
 			return [];
 		}else{
+			
 			$new_ticker = array($outCurrency => array("update" => time(), "price" => $get[$outCurrency]));
-			$dd = self::ticker_save($new_ticker, $inpCurrency);
-			// echo $new_ticker;
+			self::ticker_save($new_ticker, $inpCurrency);
+
 			return $new_ticker;
 		}
 	}
@@ -78,7 +122,7 @@ class CryptoConverter
 
 		$std_class = array($outCurrency => array("update" => time(), "price" => $ticker[$outCurrency]));
 		return $std_class;
-}
+	}
 
 	static public function ticker_save($ticker, $inpCurrency){
 		$file = file_get_contents(self::PATH_CACHE);  // Открыть файл data.json
@@ -93,6 +137,33 @@ class CryptoConverter
 		file_put_contents(self::PATH_CACHE,json_encode($taskList));
 		unset($taskList);   
 		return true;
+	}
+
+	//{"ticker" => {array($outCurrency => array("update" => time(), "price" => $ticker[$outCurrency]))}. $inpCurrency => ""}
+
+	static public function all_tickers_save($all_tickers){
+		$return_courses = array();
+		$file = file_get_contents(self::PATH_CACHE);  // Открыть файл data.json
+		$taskList = json_decode($file,TRUE);  
+
+		unset($file);
+		foreach ($all_tickers as $ticker) {
+			$get = self::get_ticker($ticker['inpCurrency'], $ticker['outCurrency']);
+			$new_ticker = array($ticker['outCurrency'] => array("update" => time(), "price" => $get[$ticker['outCurrency']]));
+			if (isset($taskList[$ticker['inpCurrency']])) {
+				$add_array = array_merge($taskList[$ticker['inpCurrency']], $new_ticker);
+			}else{
+				$add_array = $new_ticker;
+			}
+			$taskList[$ticker['inpCurrency']] = $add_array; 
+
+			array_push($return_courses, array($ticker['inpCurrency'] => $new_ticker));
+		}
+		
+		
+		file_put_contents(self::PATH_CACHE,json_encode($taskList));
+		unset($taskList);   
+		return $return_courses;
 	}
 
 
--- pages/list.php
+++ pages/list.php
@@ -1,20 +1,33 @@
 <?php 
-$outCrypto = array("BTC", "ETH", "SHIB");
+Route::$TITLE = "Курс криптовалют";
+$outCrypto = array("BTC", "ETH", "SHIB", "USDT", "USDC", "BUSD", "ADA", "LTC", "LINK", "ATOM");
 $outCurrency = array("USD", "RUB");
+// $list_ticker = array();
+// foreach($outCrypto as $crypto){
+// 	array_push($list_ticker, array("inpCurrency" => $crypto, "outCurrency" => $outCurrency[0]));
+// 	array_push($list_ticker, array("inpCurrency" => $crypto, "outCurrency" => $outCurrency[1]));
+// }
+// 	// var_dump($list_ticker);
+// 	$c = CryptoConverter::search_list_ticker($list_ticker);
+// 	echo "<pre>";
+// 	var_dump($c);
+// 	echo "</pre>";
+
+
 ?>
 <section id="converter">
 	<div class="container">
 		<h1 class="name_page">Курс криптовалют</h1>
 		<div class="main_block">
 			<table class="out_curs">
-				<tr>
+				<thead>
 				    <th>Криптовалюта</th>
 				    <th>Цена в USD</th>
 				    <th>Цена в RUB</th>
-			   </tr>
+			   </thead>
 			   <?php 
 			   	foreach($outCrypto as $crypto){
-				   	echo "<tr><td>$crypto</td>";
+				   	echo "<tbody><td>$crypto</td>";
 				   	foreach($outCurrency as $Currency){
 				  		$sum = CryptoConverter::search_ticker($crypto, $Currency);
 				  		$sum = $sum['curs'];
@@ -23,14 +36,8 @@ $outCurrency = array("USD", "RUB");
 						echo "<td>$sum</td>";
 					}
 				}
-				echo "</tr>";
+				echo "</tbody>";
 			   ?>
-
-				<tr>
-					
-					
-					<td>Цена в RUB</td>
-				</tr>
 			</table>
 			
 		</div>
